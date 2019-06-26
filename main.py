2f71cbddd05b3b54c382710b65e3b29af96f8423
russlarge256 <45131808+russlarge256@users.noreply.github.com>
2019-06-26 15:23:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,32 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
+
+@app.route('/create/', methods=["GET", "POST"])
+def update_donations():
+
+    if request.method == "GET":
+        return render_template('update.jinja2')
+
+    else:
+
+        # It should retrieve the donor from the database with the indicated name
+        donor_name = Donor.select().where(Donor.name == request.form['Name']).get()
+
+        # and create a new donation with the indicated donor and donation amount.
+        Donation.update(value=request.form['Donation'])\
+                .where(Donation.donor == donor_name)\
+                .execute()
+
+        # Then it should redirect the visitor to the home page.
+        return redirect(url_for('home'))
 
 @app.route('/donations/')
 def all():
@@ -18,6 +37,8 @@ def all():
     
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6738))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 6738))
+    # app.run(host='0.0.0.0', port=port)
 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
